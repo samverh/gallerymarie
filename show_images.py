46c82ff2af124e220adbe4defb1c0677ddb7d97f
samverh <samverhezen@hotmail.com>
2023-10-26 16:01:00 +0200

    upload

--- show_images.py
+++ show_images.py
@@ -2,7 +2,7 @@ import os
 
 
 # folder_path = "images/under_your_feet/" 
-folder_path = "images/collages/" 
+folder_path = "images/state_of_decay-bioscoop/" 
 
 def list_files(folder):
     for root, dirs, files in os.walk(folder):
