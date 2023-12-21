5bbb2d55de4bac0fa27f9c551706d37e0c584ea1
samverh <samverhezen@hotmail.com>
2023-12-21 17:21:07 +0100

    2d

--- show_images.py
+++ show_images.py
@@ -1,14 +1,17 @@
 import os
 
 
-folder_path = "images/collages/2" 
+folder_path = "images/2d/drawings" 
+
+
 
 # folder_path = "images/state_of_decay-bioscoop/" 
 
 def list_files(folder):
     for root, dirs, files in os.walk(folder):
         for file in files:
-            file_path = os.path.join(root, file)
+            file_path = os.path.join(root, file.replace(r"\\", "/"))
+
             print(f'"{file_path}",')
 
 
