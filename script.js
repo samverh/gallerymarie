2938da1d6691010f3f7aaa11d15f3f743612c161
Sam Verhezen <s.verhezen@in2intel.com>
2024-04-16 10:09:33 +0200

    lazyload

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 $(document).ready(function () {
+    
+    // lazy load all images with class lazyload
+    lazyload();
+
+    // js for navbar
     var trigger = $('.hamburger'),
     overlay = $('.overlay'),
     isClosed = false;
