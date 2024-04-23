086b611d0054850455dac0c6eaa05f74c31c2f7c
samverh <samverhezen@hotmail.com>
2024-04-23 16:11:40 +0200

    extra tabs + info button uyf

--- script.js
+++ script.js
@@ -38,4 +38,31 @@ $(document).ready(function () {
     $('[data-toggle="offcanvas"]').click(function () {
           $('#wrapper').toggleClass('toggled');
     }); 
+
+    // Get the button that opens the popup
+    var btn = document.getElementById("infoBtn");
+
+    // Get the element that represents the popup
+    var popup = document.getElementById("popup");
+
+    // Get the <span> element that closes the popup
+    var closeBtn = document.querySelector(".close-btn");
+
+    // When the user clicks on the button, open the popup
+    btn.onclick = function() {
+        popup.style.display = "flex";
+    }
+
+    // When the user clicks on <span> (x), close the popup
+    closeBtn.onclick = function() {
+        popup.style.display = "none";
+    }
+
+    // Also close the popup if the user clicks outside of the popup content
+    window.onclick = function(event) {
+        if (event.target == popup) {
+            popup.style.display = "none";
+        }
+    }
+
   });
